084dfb3627535c49ca81b58ad4683fa48d2822aa
Monika Kołtunik <monika.koltunik@gmail.com>
2017-06-18 22:28:04 +0200

    Adds final version of game loop

--- app/Main.hs
+++ app/Main.hs
@@ -1,47 +1,49 @@
 module Main where
 
-import Data.Matrix
-import Control.Monad
-import Board
-import Game
+import           Board
+import           MinMax
+import           Game
+import           Data.Matrix
+import           Data.Maybe
+import           Text.Read
 
 main :: IO ()
 main = do
-    putStrLn "Choose symbol O | X"
-    line <- getLine
-    let userSymbol :: Symbol
-        userSymbol = read line
+    let game = initGame
+    userTurn game
+    return()
 
-    putStrLn "Choose whose turn first"
-    line <- getLine
-    let firstTurn :: Symbol
-        firstTurn = read line
-
-    let game = initGame userSymbol firstTurn
-    gameLoop game
-    putStrLn "THE END"
-
-gameLoop :: Game -> IO ()
-gameLoop game = do
-    let oldBoard = board game
-    putStrLn $ showBoard oldBoard
-
+userTurn :: Game -> IO ()
+userTurn game = do
+    let prevBoard = board game
+    putStrLn $ showBoard prevBoard
     putStrLn "Your turn. Write coords separated with newline character"
-    line <- getLine
-    let x :: Int
-        x = read line
-    line <- getLine
-    let y :: Int
-        y = read line
-    let coords = (x,y)
+    pos <- readAndCheckPosition prevBoard
+    let newBoard = putSymbol O pos prevBoard
+    if isGameOver pos newBoard then putStrLn (showBoard newBoard ++ "User won") else aiTurn game { board = newBoard}
+    return ()
 
-    let newBoard = putSymbol (userSymbol game) coords oldBoard
+aiTurn :: Game -> IO ()
+aiTurn game = do
+    let prevBoard = board game
+    let (pos, newBoard) = aiMove prevBoard
+    if isGameOver pos newBoard then putStrLn (showBoard newBoard ++ "AI won") else userTurn game { board = newBoard}
+    return ()
 
-    unless (gameIsOver coords newBoard) (gameLoop game { board = newBoard})
-    putStrLn ""
+readAndCheckPosition :: Board -> IO (Int, Int)
+readAndCheckPosition board = do
+    (x,y) <- readAndParseInt
+    if x < 1 || x > 19 || y < 1 || y > 19 || getSymbol (x,y) board /= Empty
+        then putStrLn "Coords outside the board or nonempty field" >> readAndCheckPosition board
+    else return (x,y)
 
--- wrong data, nonempty field
--- read move, update board, check state, AI move, update board, check state ...
--- show result
--- exactly 5 stones (?)
--- contents
+readAndParseInt :: IO (Int, Int)
+readAndParseInt = do
+    x <- getLine
+    y <- getLine
+    case (readMaybe x, readMaybe y) of
+        (Just a, Just b) -> return (a,b)
+        _                -> putStrLn "Wrong format. Write coords separated with newline character" >> readAndParseInt
+
+
+
