0d0df06b19f2bceff4a32fc27a585e7a9b70867e
Monika Kołtunik <monika.koltunik@gmail.com>
2017-06-18 22:29:10 +0200

    Code refactoring

--- src/Game.hs
+++ src/Game.hs
@@ -1,20 +1,36 @@
 module Game
-  ( Game(..)
-  , initGame
-  ) where
+( Game(..)
+, initGame
+, isGameOver
+) where
 
-import Board
+import           Board
+import           Data.List
+import           Data.Matrix
 
-data Game = Game
-  { board :: Board
-  , aiSymbol :: Symbol
-  , userSymbol :: Symbol
-  , firstTurn :: Symbol
-  } deriving (Show)
+newtype Game = Game { board :: Board}
 
-initGame :: Symbol -> Symbol -> Game
-initGame userSymbol = Game initBoard (getOppositeSymbol userSymbol) userSymbol
+initGame :: Game
+initGame = Game initBoard
 
-getOppositeSymbol :: Symbol -> Symbol
-getOppositeSymbol X = O
-getOppositeSymbol O = X
+isGameOver :: Pos -> Board -> Bool
+isGameOver pos@(row,col) board = any (containsWinningSetting symbol) statesToCheck
+       where statesToCheck = sequencesThatContainsPosition pos board
+             symbol = getElem row col board
+
+data SeqType = I | P | S
+
+containsWinningSetting :: Symbol -> String -> Bool
+containsWinningSetting symbol statesAsString =
+                 any (`isPrefixOf` statesAsString) (generateStates symbol P) ||
+                 any (`isSuffixOf` statesAsString) (generateStates symbol S) ||
+                 any (`isInfixOf` statesAsString) (generateStates symbol I)
+
+generateStates :: Symbol -> SeqType -> [String]
+generateStates symbol t = case t of
+                            I -> [pref ++ fiveSymbols ++ suff | pref <- otherSymbols, suff <- otherSymbols]
+                            P -> [fiveSymbols ++ suff | suff <- otherSymbols]
+                            S -> [pref ++ fiveSymbols | pref <- otherSymbols]
+                            where otherSymbols = ["_", show $ getOppositeSymbol symbol]
+                                  fiveSymbols = concatListOfSymbols $ replicate 5 symbol
+
